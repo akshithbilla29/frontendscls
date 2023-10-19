624c379b377fcd02bbbd4c4c9ac885e794cae5d8
akshithbilla29 <126647081+akshithbilla29@users.noreply.github.com>
2023-10-19 08:39:49 +0530

    Add files via upload

---
+++ css/html2script.js
@@ -0,0 +1,208 @@
+function searchNames() {
+    const input = document.getElementById('s1').value.toLowerCase();
+    const buttons = document.querySelectorAll('table li button');
+
+    buttons.forEach(button => {
+        const buttonText = button.innerText.toLowerCase();
+        if (buttonText.includes(input)) {
+            button.style.display = 'block'; // Show the button if it matches the search
+        } else {
+            button.style.display = 'none';  // Hide the button if it doesn't match the search
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const htmlButton = document.getElementById("t1");
+    const cssButton = document.getElementById("t2");
+    const JavaScriptButton=document.getElementById("t3");
+    const XMLButton=document.getElementById("t4");
+    const aButton=document.getElementById("t5");
+    const a1Button=document.getElementById("t6");
+    const a2Button=document.getElementById("t7");
+    const a3Button=document.getElementById("t8");
+    const a4Button=document.getElementById("t9");
+    const a5Button=document.getElementById("t10");
+    const a6Button=document.getElementById("t11");
+    const a7Button=document.getElementById("t12");
+    const a8Button=document.getElementById("t13");
+    const a9Button=document.getElementById("t14");
+    const a10Button=document.getElementById("t15");
+    const a11Button=document.getElementById("t16");
+    const a12Button=document.getElementById("t17");
+    const a13Button=document.getElementById("t18");
+    const a14Button=document.getElementById("t19");
+    const a15Button=document.getElementById("t20");
+    const a16Button=document.getElementById("t21");
+    const a17Button=document.getElementById("t22");
+    const a18Button=document.getElementById("t23");
+    const a19Button=document.getElementById("t24");
+    const a20Button=document.getElementById("t25");
+    const a21Button=document.getElementById("t26");
+    const a22Button=document.getElementById("t27");
+    const a23Button=document.getElementById("t28");
+    const a24Button=document.getElementById("t29");
+    const a25Button=document.getElementById("t30");
+    const a26Button=document.getElementById("t31");
+    const a27Button=document.getElementById("t32");
+    const a28Button=document.getElementById("t33");
+    const a29Button=document.getElementById("t34");
+    const a30Button=document.getElementById("t35");
+    const a31Button=document.getElementById("t36");
+    const a32Button=document.getElementById("t37");
+    const a33Button=document.getElementById("t38");
+    const a34Button=document.getElementById("t39");
+    const a35Button=document.getElementById("t40");
+    const a36Button=document.getElementById("t41");
+    const a37Button=document.getElementById("t42");
+    const a38Button=document.getElementById("t43");
+    const a39Button=document.getElementById("t44");
+     
+    htmlButton.addEventListener("click", function()
+     {
+      window.location.href = "background image position.html";
+    });
+      cssButton.addEventListener("click", function() {
+      window.location.href = " css animation direction.html";
+    });
+    JavaScriptButton.addEventListener("click", function()
+     {
+      window.location.href = "css animations ex1.html";
+    });
+    XMLButton.addEventListener("click", function()
+     {
+      window.location.href = "css animations-direction images.html";
+    });
+    aButton.addEventListener("click", function()
+     {
+      window.location.href = "css animations-fill-mode.html";
+    });
+    a1Button.addEventListener("click", function()
+     {
+      window.location.href ="css animation-timing-functions.html";
+    });
+    a2Button.addEventListener("click", function()
+     {
+      window.location.href = "css background attachment.html";
+    });
+    a3Button.addEventListener("click", function()
+     {
+      window.location.href ="css background color.html";
+    });
+    a4Button.addEventListener("click", function()
+     {
+      window.location.href = "css background image.html";
+    });
+    a5Button.addEventListener("click", function()
+     {
+      window.location.href = "css background repeat.html";
+    });
+    a6Button.addEventListener("click", function()
+     {
+      window.location.href ="css background repeat-x.html";
+    });
+    a7Button.addEventListener("click", function()
+     {
+      window.location.href = "css background repeat-y.html";
+    });
+    a8Button.addEventListener("click", function()
+     {
+      window.location.href = "css border color.html";
+    });
+    a9Button.addEventListener("click", function()
+     {
+      window.location.href = " css class selector.html";
+    });
+    a10Button.addEventListener("click", function()
+     {
+      window.location.href =" css colors.html";
+    });
+    a11Button.addEventListener("click", function()
+     {
+      window.location.href ="css fonts.html";
+    });
+    a12Button.addEventListener("click", function()
+     {
+      window.location.href ="css grouping selector.html";
+    });
+    a13Button.addEventListener("click", function()
+     {
+      window.location.href = " css hex value.html";
+    });
+    a14Button.addEventListener("click", function()
+     {
+      window.location.href = " css hsl colors.html";
+    });
+    a15Button.addEventListener("click", function()
+     {
+      window.location.href = " css id selector.html";
+    });
+    a16Button.addEventListener("click", function()
+     {
+      window.location.href =" css individual padding.html";
+    });
+    a17Button.addEventListener("click", function()
+     {
+      window.location.href = "";
+    });
+    a18Button.addEventListener("click", function()
+     {
+      window.location.href =" css overflow property.html" ;
+    });
+    a19Button.addEventListener("click", function()
+    {
+     window.location.href =" css position property.html";
+   });
+   a20Button.addEventListener("click", function()
+   {
+    window.location.href = " css RGB colors.html";
+  });
+  a21Button.addEventListener("click", function()
+  {
+   window.location.href = "";
+ });
+ a22Button.addEventListener("click", function()
+ {
+  window.location.href = "";
+});
+a23Button.addEventListener("click", function()
+     {
+      window.location.href = "";
+    });
+    a24Button.addEventListener("click", function()
+    {
+     window.location.href = "";
+   });
+   aButton.addEventListener("click", function()
+   {
+    window.location.href = "";
+  });
+  aButton.addEventListener("click", function()
+  {
+   window.location.href = "";
+ });
+ aButton.addEventListener("click", function()
+ {
+  window.location.href = "";
+});
+aButton.addEventListener("click", function()
+     {
+      window.location.href = "";
+    });
+    aButton.addEventListener("click", function()
+    {
+     window.location.href = "";
+   });
+   aButton.addEventListener("click", function()
+   {
+    window.location.href = "";
+  });
+  aButton.addEventListener("click", function()
+  {
+   window.location.href = "";
+ });
+ aButton.addEventListener("click", function()
+ {
+  window.location.href = "";
+});
+  });
